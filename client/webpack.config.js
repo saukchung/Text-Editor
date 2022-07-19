563de29dd553abaf0cd0d0243e41dfda81f7c47c
Sa Uk Chung <chungsauk95@gmail.com>
2022-07-19 19:33:07 -0400

    added a blank readme and fixed the install.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -20,7 +20,7 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Webpack Plugin',
+        title: 'Just Another Text Editor',
       }),
       new InjectManifest({
         swSrc: './src-sw.js',
